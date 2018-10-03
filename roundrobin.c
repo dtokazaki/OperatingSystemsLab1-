6685e5a727b6b2d73eb8d80c6112a5015c3428c6
dtokazaki <daniel.t.okazaki@gmail.com>
2018-10-02 20:56:47 -0700

    roundrobin
    commented and fixed some bugs

--- roundrobin.c
+++ roundrobin.c
@@ -13,33 +13,36 @@ void printQueue(struct process *array) {
 	}
 }
 
-void run(struct process *array, int &pos, int &proc_left, int quanta, char *timetable) {
+void run(struct process *array, int &pos, int &proc_left, int &quanta, char *timetable) {	// main function that runs the simulation
 
-	if(array[pos]->startTime == -1)
+	if(array[pos]->startTime == -1)								// startTime initialized to -1, which means it has not been updated
 		array[pos]->startTime = quanta;
-	array[pos]->timeRemaining--;
-	if(array[pos]->timeRemaining == 0) {
+	array[pos]->timeRemaining--;								// decrement time remaining
+	if(array[pos]->timeRemaining == 0) {							// if the job is done, set the completeTime, append the name to the finished string, and decrement the amount of processes left
 		array[pos]->completeTime = quanta;
 		strcat(timetable,array[pos]->name);
 		proc_left--;
 	}
-	quanta++;
+	quanta++;										// increase the quanta
 	pos++;
+	if(pos == 10)										// if the pos reaches the end of the array, start over from the begining
+		pos = 0;
 	return;
 
 }
 
-int avail(struct process *array, int &pos)
+int avail(struct process *array, int &pos, int quanta)						// Test to see if a process is available
 {
 	int i;
 	for(i=pos;i<10;i++) {
-		if(array[i]->timeRemaining>0)
-			return i ;
-	}
-	for(i=0;i<pos;i++) {
-		if(array[i]->timeRemaining>0)
+		if(array[i]->timeRemaining>0 && array[i]->arrivalTime <= quanta)		// Process must be still running, and have arrived before the current quanta
 			return i;
 	}
+	for(i=0;i<pos;i++) {
+		if(array[i]->timeRemaining>0 array[i]->arrivalTime <= quanta)
+			return i;
+	}
+	return -1;
 }
 
 char* roundRobin(struct process *array) {
@@ -49,17 +52,23 @@ char* roundRobin(struct process *array) {
 	char* timechart;
 	int proc_left= size;
 	int pos = 0;
+	char error[2] = "*";
 
 	timechart = (char*) malloc(111*sizeof(char));						// allocate enough memory for maximum number of time slots in the CPU + 1 for null character at the end
-	memset(timechart,"\n",sizeof(timechart));							// zero out memory to prevent garbage data
+	memset(timechart,"\n",sizeof(timechart));						// zero out memory to prevent garbage data
 	
-	while(proc_left > 0) {
+	while(proc_left > 0) {									// Exit lop when no more processes left
 		
-		pos = avail(array, pos);	
-		run(array,pos,quanta,timetable);
+		pos = avail(array, pos);
+		if(pos == -1) {									// If no process is available, CPU is idle, and advance the quanta
+			quanta++;
+			strcat(timetable,error);
+		}
+		else	
+			run(array,pos,quanta,timetable);					// Only run if there is a process available, and has arrived						
 	}
 
-	printQueue(array);
+	printQueue(array);									// Print function for debugging purposes
 	return timechart;
 }
 
