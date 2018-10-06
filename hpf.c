9a6020510dfe77c9e15903b80e325918f0318cbf
Riley Bergin <rbergin@scu.edu>
2018-10-06 10:12:18 -0700

    added hpf

---
+++ hpf.c
@@ -0,0 +1,21 @@
+type struct Process {
+    char name;
+    int arrivalTime;
+    int expectedTotalRuntime;
+    int priority;
+    int startTime;
+    int completionTime;
+    int runtimeRemaining;
+};
+
+Process[] queue;
+
+char* HPF(Process[] &queue) {
+    // First, sort array by priority
+
+    // Then, sort each priority level by fcfs
+
+    // Calculate times
+
+    // create and return concatentation of names
+}
