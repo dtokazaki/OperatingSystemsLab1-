05b20fd9f1c63eb008d16a5f83256839191ecaf3
Christopher Yates <cmyates48@gmail.com>
2018-10-06 10:21:04 -0700

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,52 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <process.c>
 
-struct process {
-	char name;
-	int arrivalTime; // 0-99
-	int runTime; // 1-10
-	int priority; // 1-5
-	int startTime;
-	int completeTime;
-	int runTimeRemaining;
-};
-
-struct process processes[10];
-
-int main() {
+char * sjf(struct process *processes) {
 	
-	// provide letters
-	char alphabet[10] = {'A','B','C','D','E','F','G','H','I','J'};
-	
-	// give values to processes
 	int i;
-	srand(time(NULL));
-	for (i=0; i<10;i++){
-	    
-		int run_time = 1 + rand()%10;
-		processes[i].name = alphabet[i];
-		processes[i].arrivalTime = rand()%100;
-		processes[i].runTime = run_time;
-		processes[i].priority = (1 + rand()%6);
-		processes[i].startTime = 0;
-		processes[i].completeTime = 0;
-		processes[i].runTimeRemaining = run_time;
-	};
-	
-	// sort the process array by arrival time
-	int j;
-	for (i=0;i<10;i++){
-		for (j=0;j<10-i-1;j++){
-			if (processes[j].arrivalTime > processes[j+1].arrivalTime){
-				struct process temp = processes[j];
-				processes[j] = processes[j+1];
-				processes[j+1] = temp;
-			}
-		}
-	};
-
-    for (i=0; i<10;i++){
+    	for (i=0; i<10;i++){
 	    printf("%c, %d, %d\n", processes[i].name, processes[i].arrivalTime, processes[i].runTime);
 	}
 
@@ -56,8 +16,8 @@ int main() {
 	
 	// run the algorithm
 	for (time=0; time<100;time++) {
-		if (processes[p_index].runTimeRemaining != 0 && time>=processes[p_index].arrivalTime){		// check if process is still running
-		    printf("%c\n", processes[p_index].name);
+		if (processes[p_index].runTimeRemaining != 0 && time>=processes[p_index].arrivalTime){  // check if process is still running
+		    printf("%c\n", processes[p_index].name);		// debug to see character
 			quantum[time] = processes[p_index].name;;	// if so, label time with process name
 			processes[p_index].runTimeRemaining--;		// decrement the running time
 		}
@@ -82,7 +42,9 @@ int main() {
 				processes[p_index].runTimeRemaining--;		// decrement run time remaining for new process
 			}
 		}
-		printf("%d, %d, %c, %d\n", time, processes[p_index].runTimeRemaining, quantum[i], p_index);
+		printf("%d, %d, %c, %d\n", time, processes[p_index].runTimeRemaining, quantum[i], p_index); // check the state of every quantum
 	}
+	
+	return strdup(&processes[0]);
 
 };
